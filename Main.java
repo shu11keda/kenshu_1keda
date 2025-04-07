53c941140e9af498c64498eb9334d2ebd1a48295
*kefGGsZdGJ <ikeda.shui@f-inet.co.jp>
2025-04-07 15:40:44 +0900

    q

--- Main.java
+++ Main.java
@@ -38,6 +38,7 @@ public class Main{
         // ここから
 
         System.out.println("★ 九九表(1~9) ★");
+        // ここがfor文
         for(int i = 1;i <= 9;i++){
             for(int j = 1;j <= 9; j++){
                 System.out.print(i * j + "\t");
