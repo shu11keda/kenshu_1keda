1b2b773aa58aa00b424e17205f6d649438db02e8
*kefGGsZdGJ <ikeda.shui@f-inet.co.jp>
2025-04-07 14:34:40 +0900

    ifMan

--- Main.java
+++ Main.java
@@ -5,7 +5,17 @@ public class Main{
 
         Scanner sc = new Scanner(System.in);
         //　ここから
-            
+        if(!sc.hasNextInt()){
+                
+            System.out.println("入力されていません");
+            return;
+        }
+        int score = sc.nextInt();
+        
+        if(score >= 60){
+            System.out.println("合格");
+        }else {
+            System.out.println("不合格");
         //　ここまでifMan
 
         // ここから
