1f54fe3d5d0eaff086ce5078ab0b5ac387073e7a
*kefGGsZdGJ <ikeda.shui@f-inet.co.jp>
2025-04-07 17:18:46 +0900

    ok

--- Main.java
+++ Main.java
@@ -2,7 +2,6 @@ import java.util.*;
 
 public class Main{
     public static void main(String[] args){
-        System.out.println("Hello World");
         System.out.println("HelloWorld");
         System.out.println("I'm issue2");
         System.out.println("I'm issue3");
