6a4502a753340767f78ae4b97ac8b8f221ebd603
*kefGGsZdGJ <ikeda.shui@f-inet.co.jp>
2025-04-07 14:45:56 +0900

    forMan

--- Main.java
+++ Main.java
@@ -9,7 +9,14 @@ public class Main{
         //　ここまでifMan
 
         // ここから
+            System.out.println("★ 九九表(1~9) ★\n");
 
+            for(int i = 1;i <= 9;i++){
+                for(int j = 1;j <= 9; j++){
+                    System.out.println(i * j + "\t");
+                }
+                System.out.println();
+            }
         // ここまでforMan
 
         //　ここから
