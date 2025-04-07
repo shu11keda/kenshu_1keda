1cf11bdbe754c0c76318da8f96f8a74616dcd224
*kefGGsZdGJ <ikeda.shui@f-inet.co.jp>
2025-04-07 16:45:17 +0900

    a

--- Main.java
+++ Main.java
@@ -3,10 +3,7 @@ import java.util.*;
 public class Main{
     public static void main(String[] args){
         System.out.println("HelloWorld");
-<<<<<<< HEAD
         System.out.println("I'm issue2");
-=======
         System.out.println("I'm issue3");
->>>>>>> 9c0cbc6 (I'm issue3)
     }
 }
